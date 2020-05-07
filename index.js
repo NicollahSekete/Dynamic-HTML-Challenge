fe7de4527af8153eb355fd96975db66895a6087a
NicollahSekete <nicollahtsekete@gmail.com>
2020-05-07 12:28:46 +0100

    Thursday 7 may commit

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
-const wrapper = document.getElementById('wrapper');
-//const myfirstbox =document.getElementById('box1');
-const paragraphs = document.getElementsByTagName('p');
-const events = document.getElementById('removeBoxes');
-const images = document.images('img');
-
-//images.style.height= "calc(100vw / 3.5)";
-//images.style.width= "calc(100vw / 3.5)";
+let inImg = document.getElementById("inputImage")
+let inputBoxNum = document.getElementById("inputBoxNum")
+let imageMaker = document.createElement("img")
+let wrapper = document.getElementById("wrapper")
+const removeBoxButton = document.getElementById("button2")
+const buttonMaker = document.getElementById("button1")
 
 
- doBox.onclick = () => {
-    for (let i = paragraphs.length - 1; i >= 0; --i) {
-        paragraphs[i].remove();
-      }
-  };
 
-  boxOne.onclick = ()=> {
-    document.getElementById("box1").style.visibility = "hidden";}
-    
-    boxTwo.onclick = ()=> {
-        document.getElementById("box2").style.visibility = "hidden";}
 
-//Dynamic image 
+//how to create as many boxes as possible // dynamic image // currently showing the same image multiple times
+const createBox = () => {
+
+    for (let i = 0; i < inputBoxNum.value; i++) {
+        let box = document.createElement("div")
+        document.body.appendChild(box)
+        let imageMaker = document.createElement("img")
+        imageMaker.src = `https://source.unsplash.com/random/500x500/\?${inImg.value}`
+        imageMaker.setAttribute("height", "100%");
+        imageMaker.setAttribute("width", "100%");
+        box.appendChild(imageMaker)
+        console.log(box)
+        box.classList.add('box')
+        wrapper.appendChild(box)
+    }
+}
+
+
+//button to remove boxes
+removeBoxButton.onclick = () => {
+    box.classList.toggle('hide')
+}
+
+
+//button to click to grab images. 
+buttonMaker.onclick =() => {
+    event.preventDefault()
+    console.log(inputBoxNum.value)
+    createBox();
+
+}
